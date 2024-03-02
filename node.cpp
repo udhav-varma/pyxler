eee570bf05d01895361f801430061f0f05f4c3a2
Yuvraj <yuvrajk@cse.iitk.ac.in>
2024-03-02 21:50:20 +0530

    Added graphviz code

--- node.cpp
+++ node.cpp
@@ -2,9 +2,8 @@
 
 using namespace std;
 
-
 struct node{
-    int id;
+    int id = -1;
     node* parent;     // index of parent node
     vector<node*> children;   // indexes of children nodes
     string type, name;
@@ -22,19 +21,47 @@ struct AST{
         sz++;
     }
 
-    void add_edge(node * parent, node* child){
+    void add_edge(node* parent, node* child){
         parent->children.push_back(child);
         child->parent = parent;
     }
 
-    void dfs(int u){
+    string intToString(int x){
+        string s = "";
+        while(x){
+            s.push_back(x%10 + '0');
+            x /= 10;
+        }
+        reverse(s.begin(), s.end());
+        return s;
+    };
 
+    void dfs1(node* node, int &i){
+        if(node->id==-1){
+            node->id = i++;
+            string s = intToString(node->id) + "[label=\"" + node->name + "\"]\n";
+            dotcode.append(s);
+        }
+        for(auto u: node->children){
+            dfs1(u, i);
+        }
+    }
+
+    void dfs2(node* node){
+        for(auto u: node->children){
+            string s1 = intToString(node->id) + " -> " + intToString(u->id) + "\n";
+            dotcode.append(s1);
+            dfs2(u);
+        }
     }
 
     void graphviz(){
-        dotcode.append("digraph AST{");
-        dfs(0);
-        dotcode.append("}");
+        int id = 1;
+        dotcode.append("digraph AST{\n");
+        dfs1(tree[1], id);
+        dfs2(tree[1]);
+        dotcode.append("}\n");
+        cout<<dotcode;
     }
 
 };
